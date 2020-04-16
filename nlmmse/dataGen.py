8187c57e56668ff2ec1090f8ada1a3e2a2797020
s-a-n-d-y <mr.sandipan.das@gmail.com>
2020-04-16 18:40:05 +0200

    NLMMSE

--- nlmmse/dataGen.py
+++ nlmmse/dataGen.py
@@ -1,7 +1,4 @@
-import numpy as np, numpy.random
-from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 from scipy.linalg import block_diag
 
 # A, known random matrix of dim qxp
@@ -42,8 +39,10 @@ class DataGen:
     def generate_gmm_samples(self):
         A = np.random.normal(0, 1, (self.n_dim, self.n_variate))      
         S = np.zeros(shape=[self.n_mixtures, self.n_samples, self.n_variate])
+        X_GMM = np.zeros(shape=[self.n_mixtures, self.n_samples, self.n_dim])
         X = np.zeros(shape=[self.n_samples, self.n_dim])
         W = np.zeros(shape=[self.n_samples, self.n_dim])
+
 
         for i in range(self.n_dim):
             # W = error matrix, 0 mean std. dev = 1
@@ -52,11 +51,11 @@ class DataGen:
         # Multiply each mixture by alpha, where sum(alpha) = 1, generated from Dirichlet
         for i in range(self.n_mixtures):
             S[i,:,:] = self.alpha[:,i].item()*np.random.multivariate_normal(self.mu[i,:], self.cov[i,:,:], self.n_samples)
-            X += np.dot(S[i,:,:], A.T) + W
+            X_GMM[i,:,:] = np.dot(S[i,:,:], A.T) + W
+            X += X_GMM[i,:,:]
         
         # Observed samples, transformation (NN), original feature distribution, noise
-        return X, A, S, W
-
+        return X_GMM, X, A, S, W
 
     '''
     # Returns randomly generated samples X
@@ -105,57 +104,6 @@ class DataGen:
             plt.show()
 
         return cov
-
-
-# Main method
-np.random.seed(256)
-
-# 10000 samples, 40 variate feature, 5 mixtures, 2 dimensional data
-data = DataGen(n_samples=10000, n_variate=40, n_mixtures=10, n_dim=2, mixture_weight_random=False, cov_pos_semidefinite=True)
-print(data.mu)
-# x = obs samples (sample x dimension)
-# a = random matrix (Can be thought of input vector, known)
-# s = feature transformation matrix, n_dim x n_sample (Can be thought as weight matrix)
-# w = noise matrix
-x, a, s, w = data.generate_gmm_samples()
-
-if data.n_dim == 1:
-    # 1D plot
-    plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.hist(x, bins=data.n_samples)
-    plt.subplot(2, 1, 2)
-    for i in range(data.n_mixtures):
-        plt.hist(s[i,:,0], bins=data.n_samples)
-    plt.show()
-
-elif data.n_dim == 2:
-    #2-D plot
-    fig = plt.figure()
-    ax = fig.add_subplot(211)
-    ax.scatter(x[:, 0], x[:, 1], marker='o')
-
-    ax = fig.add_subplot(212)
-    for i in range(data.n_mixtures):
-        ax.scatter(s[i,:,0], s[i,:,1], marker='o', cmap='virdis', s=25, edgecolor='k')
-
-    plt.show()
-
-elif data.n_dim >=3:
-    #3-D plot
-    fig = plt.figure()
-    ax = fig.add_subplot(211, projection='3d')
-    ax.scatter(x[:, 0], x[:, 1], x[:, 2], marker='o')
-
-    ax = fig.add_subplot(212, projection='3d')
-    for i in range(data.n_mixtures):
-        ax.scatter(s[i,:,0], s[i,:,1], s[i,:,2], marker='o', cmap='virdis', s=25, edgecolor='k')
-
-    plt.show()
-else:
-    print("No other choice!")
-
-
 
 
 
---
+++ nlmmse/main.py
@@ -0,0 +1,65 @@
+import numpy as np
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from dataGen import DataGen
+def main():
+    
+    # Main method
+    np.random.seed(256)
+
+    # 10000 samples, 40 variate feature, 10 mixtures, 3 dimensional data
+    data = DataGen(n_samples=10000, n_variate=40, n_mixtures=10, n_dim=3, mixture_weight_random=False, cov_pos_semidefinite=True)
+    # print(data.mu)
+
+    # x_gmm = weighted Pdf of the observed samples per mixture
+    # x = obs samples (sample x dimension)
+    # a = random matrix (Can be thought of input vector, known)
+    # s = feature transformation matrix, n_dim x n_sample (Can be thought as weight matrix)
+    # w = noise matrix
+    x_gmm, x, a, s, w = data.generate_gmm_samples()
+    # Normalize between -1 and 1
+    x = x - x.mean()
+    x = x / x.max()
+
+    if data.n_dim == 1:
+        # 1D plot
+        plt.figure()
+        plt.subplot(2, 1, 1)
+        plt.hist(x, bins=data.n_samples)
+        plt.subplot(2, 1, 2)
+        for i in range(data.n_mixtures):
+            plt.hist(s[i,:,0], bins=data.n_samples)
+        plt.show()
+
+    elif data.n_dim == 2:
+        #2-D plot
+        fig = plt.figure()
+        ax = fig.add_subplot(211)
+        ax.scatter(x[:, 0], x[:, 1], marker='o')
+
+        ax = fig.add_subplot(212)
+        for i in range(data.n_mixtures):
+            ax.scatter(s[i,:,0], s[i,:,1], marker='o', cmap='virdis', s=25, edgecolor='k')
+
+        plt.show()
+
+    elif data.n_dim >=3:
+        #3-D plot
+        fig = plt.figure()
+        ax = fig.add_subplot(211, projection='3d')
+        ax.scatter(x[:, 0], x[:, 1], x[:, 2], marker='o')
+        ax.set_title('Transformed data')
+
+        ax = fig.add_subplot(212, projection='3d')
+        for i in range(data.n_mixtures):
+            ax.scatter(s[i,:,0], s[i,:,1], s[i,:,2], marker='o', cmap='virdis', s=25, edgecolor='k')
+        ax.set_title('Original normalized mixtures of the GMM')
+
+        plt.show()
+    else:
+        print("No other choice!")
+
+
+if __name__ == "__main__":
+    main()
