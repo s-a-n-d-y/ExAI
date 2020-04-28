5679ddb5c9fecf54fb46842b2b70361d27d4d3d5
s-a-n-d-y <mr.sandipan.das@gmail.com>
2020-04-28 16:19:30 +0200

    ELM code

---
+++ nlmmse/matlab/elm/ELM.m
@@ -0,0 +1,54 @@
+function [T_hat, Tt_hat]=ELM(X, T, Xt, Tt, lam, NumNodes)
+
+% % % % % % % Inputs % % % % % % % %
+% X: Train input
+% T: Train target
+% Xt: Test input
+% Tt: Test Target
+% lam: regularization hyperparameter
+% NumNodes: Number of hidden neurons
+
+% % % % % % % Outputs % % % % % % % 
+% T_hat: Train target estimate
+% Tt_hat: Test target estimate
+
+%%
+a_leaky_RLU=0;      %   set to a small non-zero value if you want to test leaky-RLU
+g=@(x) x.*(x >= 0)+a_leaky_RLU*x.*(x < 0);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Training Phase %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Input layer weight matrix: W
+[P,N]=size(X);
+W=2*rand(NumNodes,P)-1;
+b=2*rand(NumNodes,1)-1;
+ind=ones(1,N);
+B=b(:,ind);
+Z=W*X+B;
+
+%% Hidden neorons
+Y=g(Z);
+
+% % Output layer weight matrix: O
+if NumNodes < N
+    O=(T*Y')/(Y*Y'+lam*eye(size(Y,1)));
+else
+    O=(T/(Y'*Y+lam*eye(size(Y,2))))*Y';
+end
+
+T_hat=O*Y;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Testing Phase %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+Nt=size(Xt,2);
+ind=ones(1,Nt);
+B=b(:,ind);
+Zt=W*Xt+B;
+
+Yt=g(Zt);
+Tt_hat=O*Yt;
+
+return
